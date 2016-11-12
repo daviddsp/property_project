ea106f3b7f69627ea078980a10c7cdafccd1be38
David Solórzano <abdsp22@gmail.com>
2016-11-12 03:14:18 -0400

    add method update, delete PropertyController

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -25,15 +25,6 @@ class PropertyController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -76,17 +67,11 @@ class PropertyController extends Controller
     public function show($id)
     {
         //
-    }
+        $property = Property::find($id);
+        return Response::json([
+            'data' => $property
+        ], 200);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
@@ -99,6 +84,12 @@ class PropertyController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $property = Property::findOrFail($id);
+        $input = $request->all();
+        $property->fill($input)->save();
+        return Response::json([
+            'data' => $property
+        ], 200);
     }
 
     /**
@@ -110,6 +101,13 @@ class PropertyController extends Controller
     public function destroy($id)
     {
         //
+        $property = Property::find($id);
+        $property->delete();
+
+        return Response::json([
+            'data' => $property
+        ], 200);
+
     }
 
     private function vadationStore($input)
